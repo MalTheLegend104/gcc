9d030007cc904a3b7352cb4e1bfcf46d7a050900
MalTheLegend104 <79492634+MalTheLegend104@users.noreply.github.com>
2026-08-04 17:21:26 -0400

    changed to explicitly disallow dynamic linking and PIE linking

--- gcc/config/wallos/wallos.h
+++ gcc/config/wallos/wallos.h
@@ -3,24 +3,54 @@
 
 #define TARGET_WALLOS 1
 
-#undef TARGET_OS_CPP_BUILTINS
 /* Additional predefined macros. */
-#define TARGET_OS_CPP_BUILTINS()      \
-  do {                                \
-    builtin_define ("__unix__");      \
-    builtin_define ("__wallos__");      \
-    builtin_assert ("system=unix");   \
-    builtin_assert ("system=wallos");   \
-    builtin_assert ("system=posix");   \
-  } while(0);
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()        \
+  do {                                  \
+    builtin_define("__wallos__");       \
+    builtin_define("__unix__");         \
+    builtin_define("__ELF__");          \
+    builtin_assert("system=wallos");    \
+    builtin_assert("system=unix");      \
+    builtin_assert("system=posix");     \
+  } while (0)
 
-
-/* Where startup files (crt0, crti, etc.) are found */
+/* Where startup files are searched for. */
 #undef STARTFILE_PREFIX_SPEC
 #define STARTFILE_PREFIX_SPEC "/lib/"
 
-/* Default dynamic linker */
+#undef STANDARD_STARTFILE_PREFIX_1
 #define STANDARD_STARTFILE_PREFIX_1 "/lib/"
+
+#undef STANDARD_STARTFILE_PREFIX_2
 #define STANDARD_STARTFILE_PREFIX_2 "/usr/lib/"
+
+/* Startup objects. */
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC \
+  "/lib/crt1.o%s /lib/crti.o crtbegin.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC \
+  "crtend.o%s /lib/crtn.o"
+
+/* Default libraries. */
+#undef LIB_SPEC
+#define LIB_SPEC "-lc"
+
+/*
+ * WallOS currently supports only statically linked ELF executables.
+ */
+#undef LINK_SPEC
+#define LINK_SPEC \
+  "%{shared:%eWallOS does not support shared libraries} " \
+  "%{pie:%eWallOS does not support PIE executables} " \
+  "%{static-pie:%eWallOS does not support static PIE}"
+
+/*
+ * Force GCC to add the '.section .note.GNU-stack,"",@progbits' at the end of assembly outputs
+ */
+#undef TARGET_ASM_FILE_END
+#define TARGET_ASM_FILE_END file_end_indicate_exec_stack
 
 #endif /* GCC_WALLOS_H */
